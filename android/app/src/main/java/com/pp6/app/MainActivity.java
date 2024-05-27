37a3204af8f564c63567bde081ab56a376b1dcd6
LapMarcos <marcoslaporte2015@gmail.com>
2024-05-27 15:32:32 -0300

    Base

---
+++ android/app/src/main/java/com/pp6/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pp6.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'PPS-PP-App6',
+  appId: 'com.pp6.app',
+  appName: 'Conversando',
   webDir: 'www'
 };
 
